17e65fe083b294745c58f7bc42b94fd980c97da5
Judith <rvacanti@gmail.com>
2022-05-25 01:12:17 +0000

    fix path

--- util/env.py
+++ util/env.py
@@ -1,10 +1,13 @@
+import os
 import re
 
 def getEnvFile():
     vars = {}
-    with open('../.env') as file:
+    with open(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '.env'))) as file:
         for line in file:
             var = re.sub(r"[\n\t\s]*", "", line).split('=')
-            key = var[0]
-            val = var[1]
-            vars[key] = val
+            if(len(var) > 1):
+                key = var[0]
+                val = var[1]
+                vars[key] = val
+    return vars
